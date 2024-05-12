accf9a37aa18943a5c305c6f6c9bfb65629ce7e0
Sami AGOURRAM <agourram.ma@gmail.com>
2024-05-12 15:18:53 +0200

    initial implementation of base, ensemble, model_selection, preprocessing, and helper functions in utils

---
+++ preprocessing/encoding/label_encoder.py
@@ -0,0 +1,122 @@
+
+from utils._encode import _encode, _unique
+from utils.__validation import column_or_1d, _num_samples
+
+from base.base import BaseEstimator, TransformerMixin
+
+import numpy as np
+
+
+
+class LabelEncoder(TransformerMixin, BaseEstimator):
+    """Encode target labels with value between 0 and n_classes-1.
+
+    This transformer should be used to encode target values, *i.e.* `y`, and
+    not the input `X`.
+
+    Read more in the :ref:`User Guide <preprocessing_targets>`.
+
+    .. versionadded:: 0.12
+
+    Attributes
+    ----------
+    classes_ : ndarray of shape (n_classes,)
+        Holds the label for each class.
+
+    See Also
+    --------
+    OrdinalEncoder : Encode categorical features using an ordinal encoding
+        scheme.
+    OneHotEncoder : Encode categorical features as a one-hot numeric array.
+
+    Examples
+    --------
+    `LabelEncoder` can be used to normalize labels.
+
+    >>> from sklearn.preprocessing import LabelEncoder
+    >>> le = LabelEncoder()
+    >>> le.fit([1, 2, 2, 6])
+    LabelEncoder()
+    >>> le.classes_
+    array([1, 2, 6])
+    >>> le.transform([1, 1, 2, 6])
+    array([0, 0, 1, 2]...)
+    >>> le.inverse_transform([0, 0, 1, 2])
+    array([1, 1, 2, 6])
+
+    It can also be used to transform non-numerical labels (as long as they are
+    hashable and comparable) to numerical labels.
+
+    >>> le = LabelEncoder()
+    >>> le.fit(["paris", "paris", "tokyo", "amsterdam"])
+    LabelEncoder()
+    >>> list(le.classes_)
+    ['amsterdam', 'paris', 'tokyo']
+    >>> le.transform(["tokyo", "tokyo", "paris"])
+    array([2, 2, 1]...)
+    >>> list(le.inverse_transform([2, 2, 1]))
+    ['tokyo', 'tokyo', 'paris']
+    """
+
+    def fit(self, y):
+        """Fit label encoder.
+
+        Parameters
+        ----------
+        y : array-like of shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        self : returns an instance of self.
+            Fitted label encoder.
+        """
+        y = column_or_1d(y, warn=True)
+        self.classes_ = _unique(y)
+        return self
+
+    def fit_transform(self, y):
+        """Fit label encoder and return encoded labels.
+
+        Parameters
+        ----------
+        y : array-like of shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        y : array-like of shape (n_samples,)
+            Encoded labels.
+        """
+        y = column_or_1d(y, warn=True)
+        self.classes_, y = _unique(y, return_inverse=True)
+        return y
+
+    def transform(self, y):
+        """Transform labels to normalized encoding.
+
+        Parameters
+        ----------
+        y : array-like of shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        y : array-like of shape (n_samples,)
+            Labels as normalized encodings.
+        """
+        self.check_is_fitted()
+        y = column_or_1d(y, dtype=self.classes_.dtype, warn=True)
+        # transform of empty array is empty array
+        if _num_samples(y) == 0:
+            return np.array([])
+
+        return _encode(y, uniques=self.classes_)
+    
+#class LabelEncoder():
+    #def fit(self, y):
+        #self.classes_ = np.unique(y)
+        #return self
+
+    #def transform(self, y):
+        #return np.searchsorted(self.classes_, y)
--- utils/__validation.py
+++ utils/__validation.py
@@ -1,6 +1,9 @@
 # mini_sklearn/utils/validation.py
 
 import numpy as np
+import warnings
+import numbers
+
 
 def _check_sample_weight(sample_weight, X):
     """Validate sample weights.
@@ -56,3 +59,75 @@ def _check_partial_fit_first_call(estimator, classes):
     return getattr(estimator, "classes_", None) is None
 
 
+def column_or_1d(y, *, dtype=None, warn=False):
+    """Convert input data to a 1d numpy array.
+
+    Parameters
+    ----------
+    y : array-like
+        Input data.
+
+    dtype : data-type, default=None
+        Data type for `y`.
+
+    warn : bool, default=False
+        Whether to display warnings.
+
+    Returns
+    -------
+    y : ndarray
+        Output data.
+
+    Raises
+    ------
+    ValueError
+        If `y` is not a 1D array or a 2D array with a single row or column.
+
+    Examples
+    --------
+    >>> column_or_1d([1, 1])
+    array([1, 1])
+    """
+    y = np.asarray(y, dtype=dtype)
+
+    if y.ndim == 1:
+        return y.ravel()
+    elif y.ndim == 2 and (y.shape[0] == 1 or y.shape[1] == 1):
+        if warn:
+            warnings.warn("A column-vector y was passed when a 1d array was expected. Please change the shape of y to (n_samples, ).", UserWarning, stacklevel=2)
+        return y.ravel()
+
+    raise ValueError(f"y should be a 1d array, got an array of shape {y.shape} instead.")
+
+
+
+def _num_samples(x):
+    """Return number of samples in array-like x."""
+    if hasattr(x, "__len__"):
+        return len(x)
+    elif hasattr(x, "shape") and x.shape is not None:
+        if len(x.shape) == 0:
+            raise TypeError("Singleton array %r cannot be considered a valid collection." % x)
+        elif isinstance(x.shape[0], numbers.Integral):
+            return x.shape[0]
+    raise TypeError("Expected sequence or array-like, got %s" % type(x))
+
+
+
+def check_array(array, *, ensure_2d=True, ensure_min_samples=1, ensure_min_features=1):
+    if ensure_2d and array.ndim < 2:
+        raise ValueError("Expected 2D array, got array with ndim=%d" % array.ndim)
+
+    if ensure_min_samples > 0 and len(array) < ensure_min_samples:
+        raise ValueError(
+            "Found array with %d sample(s) while a minimum of %d is required"
+            % (len(array), ensure_min_samples)
+        )
+
+    if ensure_min_features > 0 and array.ndim == 2 and array.shape[1] < ensure_min_features:
+        raise ValueError(
+            "Found array with %d feature(s) while a minimum of %d is required"
+            % (array.shape[1], ensure_min_features)
+        )
+
+    return array
